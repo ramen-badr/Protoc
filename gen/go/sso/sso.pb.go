b511facfb830bfd0891b453846db97b1b1f868a1
ErzhanKorzhan <r.badyrtenov1@g.nsu.ru>
2025-05-13 22:49:09 +0700

    proto module finish

--- gen/go/sso/sso.pb.go
+++ gen/go/sso/sso.pb.go
@@ -341,7 +341,7 @@ const file_sso_sso_proto_rawDesc = "" +
 	"\x04Auth\x129\n" +
 	"\bRegister\x12\x15.auth.RegisterRequest\x1a\x16.auth.RegisterResponse\x120\n" +
 	"\x05Login\x12\x12.auth.LoginRequest\x1a\x13.auth.LoginResponse\x126\n" +
-	"\aIsAdmin\x12\x14.auth.IsAdminRequest\x1a\x15.auth.IsAdminResponseB\x14Z\x12ramen.sso.v1;ssov1b\x06proto3"
+	"\aIsAdmin\x12\x14.auth.IsAdminRequest\x1a\x15.auth.IsAdminResponseB/Z-github.com/ramen-badr/Protoc/gen/go/sso;ssov1b\x06proto3"
 
 var (
 	file_sso_sso_proto_rawDescOnce sync.Once
